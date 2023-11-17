9c4140f17466257b24466d02cce33403eee2759a
chanyinwing <katuechan@gmail.com>
2023-11-17 02:02:35 -0500

    update

--- models/todo.model.js
+++ models/todo.model.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const TodoSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean,
+    task: String,
 })
 
 const Todo = mongoose.model('Todo', TodoSchema)
 
 export default Todo
+
